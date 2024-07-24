c4b038a717f1de0b5bcb9756796f399fa6d80674
WindClan <monstertoast2@gmail.com>
2024-07-24 14:37:25 -0400

    try and fix neofetch clone

--- packages/neofetch.lua
+++ packages/neofetch.lua
@@ -13,15 +13,19 @@ local function printSeparator(char)
 end
 function drawBIMG(bimg, x, y, mon)
   local frame = bimg[1]
+  local sizeX,sizeY = term.getSize()
   for k,line in pairs(frame) do
       mon.setCursorPos(x,y+k-1)
+	  if y+k-1 > sizeY then
+		term.scroll(1)
+		mon.setCursorPos(x,sizeY)
+	  end
       mon.blit(line[1], line[2], line[3])
   end
   sleep(1)
 end
 local art_pos_x, art_pos_y = term.getCursorPos()
 local bimg = {{{"\128\143\156\143\143\147\143\128","0f000ff0","f0fff00f"},{"\151\135\128\128\128\128\139\148","f0000000","0fffffff"},{"\151\128\128\128\128\128\128\148","00000007","3ffffff0"},{"\138\128\149\128\128\128\139\133","30f000f7","0f000000"},{"\138\139\133\128\128\128\128\133","0ff00000","f000000f"},{"\128\131\143\128\128\143\131\128","00000000","ff8008ff"},}}
-drawBIMG(bimg, art_pos_x, art_pos_y, term)
 term.setCursorPos(art_pos_x+8, art_pos_y)
 --math.floor((51 - #text) / 2)
 term.setTextColor(kernel.currentUserColor())
@@ -42,3 +46,5 @@ local hours = math.floor(uptime / 3600)
 local minutes = math.floor((uptime % 3600) / 60)
 local seconds = math.floor(uptime % 60)
 leftpad("Uptime: " .. string.format("%02d:%02d:%02d", hours, minutes, seconds), leftpadsize)
+drawBIMG(bimg, art_pos_x, art_pos_y, term)
+term.setCursorPos(1, art_pos_y+8)
